b2896c3c2849e78fae22ae9cf5a3d679a182b1c5
Aditya_180859 <56697404+Aditya-180859@users.noreply.github.com>
2021-09-21 08:37:10 +0530

    Add files via upload

---
+++ binary search.py
@@ -0,0 +1,27 @@
+n = int(input("enter number of array in list: "))
+arr=[]
+
+print("enter the element:")
+for i in range(n):
+    a=int(input())
+    arr.append(a)
+
+search = int(input("enter the element to search"))
+
+flag=1
+c=0
+
+a=arr[0]
+b=arr[n-1]
+
+while(a<b):
+    mid = n // 2
+    if(arr[mid]==search):
+        print("found at",mid+1)
+        break
+
+    elif(arr[mid]>search):
+        b=arr[n-1]
+
+    else:
+        a=arr[n+1]
